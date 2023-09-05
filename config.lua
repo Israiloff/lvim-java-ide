65ca8da0c4c0d03402c137f94b0bf2c3aecf1bc6
Israiloff <stardustindark@gmail.com>
2023-09-05 15:21:08 +0500

    removed: stability related configs;

--- config.lua
+++ config.lua
@@ -2,10 +2,5 @@ lvim.plugins = {
   "mfussenegger/nvim-jdtls",
 }
 
-if lvim.builtin.dap.active then
-  require("user.dap").config()
-end
-
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 
-require("user.null_ls").config()
