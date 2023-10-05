13ce4fd8cdeff65d6896a91b69cfd7bd6dabdd73
Israiloff <stardustindark@gmail.com>
2023-10-05 18:59:56 +0500

    fixed: tabs

--- lua/local/gui.lua
+++ lua/local/gui.lua
@@ -5,7 +5,7 @@ vim.opt.relativenumber = true
 
 lvim.colorscheme = "darcula-solid"
 
--- vim.opt.tabstop = 4
--- vim.opt.softtabstop = 4
--- vim.opt.shiftwidth = 4
--- vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
