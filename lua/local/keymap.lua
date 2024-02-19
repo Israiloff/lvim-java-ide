feb8a0808459c142bd8b5a4bd7a57ac8ee73a575
Israiloff <stardustaindark@gmail.com>
2024-02-19 14:03:52 +0000

    added: chatgpt keymappings;

--- lua/local/keymap.lua
+++ lua/local/keymap.lua
@@ -15,3 +15,23 @@ lvim.keys.normal_mode["<F9>"] = "<Cmd>lua require('dap').continue()<CR>"
 lvim.keys.normal_mode["<F10>"] = "<Cmd>lua require('dap').disconnect()<CR>"
 
 lvim.keys.normal_mode["<S-TAB>"] = "<Cmd>BufferLineCycleNext<CR>"
+
+
+-- ChatGPT mappings
+lvim.builtin.which_key.mappings["C"] = {
+    name = "ChatGPT",
+    mode = { "n", "v" },
+    c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+    g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+    t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+    k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+    d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+    a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+    o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+    s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+    f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+    x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+    r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+    l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+}
