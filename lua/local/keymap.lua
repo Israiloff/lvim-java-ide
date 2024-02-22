871b39631f1bc09538c1d8eca998e45a04ea2932
Israiloff <stardustaindark@gmail.com>
2024-02-22 12:06:27 +0500

    added: lsp mappings for java;

--- lua/local/keymap.lua
+++ lua/local/keymap.lua
@@ -35,3 +35,12 @@ lvim.builtin.which_key.mappings["C"] = {
     r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
     l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
 }
+
+-- LSP mappings
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references - Java", expr = true, silent = true })
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set('n', '<M-p>', vim.lsp.buf.signature_help, { desc = "Signature help" })
+vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+vim.keymap.set('n', '<M-i>', vim.lsp.buf.hover, { desc = "Show hover" })
