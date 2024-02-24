67f87d917940874cb5d6e919999dccb3dc5188f4
Israiloff <stardustindark@gmail.com>
2024-02-25 01:29:16 +0500

    fixed: nvim tree appearance;

--- lua/local/gui.lua
+++ lua/local/gui.lua
@@ -9,3 +9,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+lvim.builtin.nvimtree.setup.view.relativenumber = true
+lvim.builtin.nvimtree.setup.view.adaptive_size = true
