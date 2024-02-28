5083e6fbc3492faafc0c56725ed14119ec987210
Israiloff <stardustindark@gmail.com>
2024-02-28 11:50:06 +0500

    added: cb clipboard provider setup;

---
+++ lua/local/clipboard.lua
@@ -0,0 +1,13 @@
+vim.g.clipboard = {
+  name = "cb",
+  copy = {
+    ["+"] = "cb copy",
+    ["*"] = "cb copy",
+  },
+  paste = {
+    ["+"] = "cb paste",
+    ["*"] = "cb paste",
+  },
+  cache_enabled = 0,
+}
+
