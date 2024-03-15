8ae1ecf84fa53ed8e17def3725001c3b59a99930
Israiloff <stardustindark@gmail.com>
2024-03-15 17:30:23 +0500

    changed: markdown language server to remark ls;

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,1 +1,10 @@
-require("lvim.lsp.manager").setup("marksman")
+lspconfig = require("lspconfig")
+
+lspconfig.remark_ls.setup {
+    root_dir = lspconfig.util.root_pattern(".git", ".remarkrc", "package.json", "pom.xml", ".settings"),
+    settings = {
+        requireConfig = true
+    }
+}
+
+vim.cmd("LspStart remark_ls")
