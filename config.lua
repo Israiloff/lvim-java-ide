a8e08b475fb6feec75806a7ffdb520c0b3d9bf6d
Israiloff <stardustindark@gmail.com>
2024-03-17 18:54:08 +0500

    fixed: plugins' defining point;

--- config.lua
+++ config.lua
@@ -1,40 +1,4 @@
-lvim.plugins = {
-    {
-        "mfussenegger/nvim-jdtls",
-    },
-    {
-        "israiloff/darcula-java",
-        dependencies = "rktjmp/lush.nvim"
-    },
-    {
-        "petertriho/nvim-scrollbar",
-    },
-    {
-        "danymat/neogen",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-    },
-    {
-        "jackMort/ChatGPT.nvim",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
-            "folke/trouble.nvim",
-            "nvim-telescope/telescope.nvim"
-        }
-    },
-    {
-        "github/copilot.vim"
-    },
-    {
-        "dawsers/telescope-file-history.nvim"
-    },
-    {
-        "olimorris/persisted.nvim",
-        lazy = false,
-        config = true,
-    }
-}
-
+require("local.plugins")
 require("local.lsp")
 require("local.gui")
 require("local.keymap")
---
+++ lua/local/plugins.lua
@@ -0,0 +1,36 @@
+lvim.plugins = {
+    {
+        "mfussenegger/nvim-jdtls",
+    },
+    {
+        "israiloff/darcula-java",
+        dependencies = "rktjmp/lush.nvim"
+    },
+    {
+        "petertriho/nvim-scrollbar",
+    },
+    {
+        "danymat/neogen",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+    },
+    {
+        "jackMort/ChatGPT.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "folke/trouble.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    },
+    {
+        "github/copilot.vim"
+    },
+    {
+        "dawsers/telescope-file-history.nvim"
+    },
+    {
+        "olimorris/persisted.nvim",
+        lazy = false,
+        config = true,
+    }
+}
