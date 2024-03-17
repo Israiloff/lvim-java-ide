9b7e63962e28d9504e1c83136b80b601c185546b
Israiloff <stardustindark@gmail.com>
2024-03-17 19:12:31 +0500

    fixed: descrition issues;

--- lua/local/keymap.lua
+++ lua/local/keymap.lua
@@ -38,7 +38,7 @@ lvim.builtin.which_key.mappings["a"] = {
 }
 
 -- LSP mappings
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references - Java", expr = true, silent = true })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references", expr = true, silent = true })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
