dac2531e3dc4467115a78fa083a3639bf515592a
Israiloff <stardustindark@gmail.com>
2024-03-17 19:49:38 +0500

    fixed: keymaps related code refactoring issues;

--- lua/local/keymap.lua
+++ lua/local/keymap.lua
@@ -39,12 +39,12 @@ lvim.builtin.which_key.mappings["a"] = {
 
 -- LSP mappings
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references", expr = true, silent = true })
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
-vim.keymap.set('n', '<M-p>', vim.lsp.buf.signature_help, { desc = "Signature help" })
-vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { desc = "Go to type definition" })
-vim.keymap.set('n', '<M-i>', vim.lsp.buf.hover, { desc = "Show hover" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "<M-p>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+vim.keymap.set("n", "<M-i>", vim.lsp.buf.hover, { desc = "Show hover" })
 
 -- Copilot mappings
 vim.api.nvim_set_keymap("i", "<M-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
