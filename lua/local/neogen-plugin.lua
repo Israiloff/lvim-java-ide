f3f6cc6f40437c9363a29587a3a2e9883a8b98b4
Israiloff <stardustindark@gmail.com>
2024-03-17 18:48:01 +0500

    fixed: neogen plugin's defenitions;

--- lua/local/neogen-plugin.lua
+++ lua/local/neogen-plugin.lua
@@ -1,3 +1,9 @@
+require('neogen').setup {
+    enabled = true,             --if you want to disable Neogen
+    input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+    -- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
+}
+
 local wkstatus_ok, which_key = pcall(require, "which-key")
 if not wkstatus_ok then
     return
