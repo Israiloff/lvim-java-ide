b9c5749564730f50704830e59bbc05a909c057a4
Israiloff <stardustindark@gmail.com>
2024-03-18 01:12:46 +0500

    added: sessions button into the dashboard;

---
+++ lua/local/alpha.lua
@@ -0,0 +1,15 @@
+lvim.builtin.alpha.dashboard.section.buttons.entries[8] =
+    lvim.builtin.alpha.dashboard.section.buttons.entries[7]
+lvim.builtin.alpha.dashboard.section.buttons.entries[7] =
+    lvim.builtin.alpha.dashboard.section.buttons.entries[6]
+lvim.builtin.alpha.dashboard.section.buttons.entries[6] =
+    lvim.builtin.alpha.dashboard.section.buttons.entries[5]
+lvim.builtin.alpha.dashboard.section.buttons.entries[5] =
+    lvim.builtin.alpha.dashboard.section.buttons.entries[4]
+lvim.builtin.alpha.dashboard.section.buttons.entries[4] =
+    lvim.builtin.alpha.dashboard.section.buttons.entries[3]
+lvim.builtin.alpha.dashboard.section.buttons.entries[3] = {
+    "s",
+    lvim.icons.ui.Files .. "  Sessions",
+    "<cmd>Telescope persisted<CR>"
+}
