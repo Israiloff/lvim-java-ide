26bd3147c31557b35fa1767644065847880f42ba
Israiloff <stardustindark@gmail.com>
2024-03-17 20:25:22 +0500

    fixed: lsp, dap and other keymappings setup and defenitions;

--- lua/local/keymap.lua
+++ lua/local/keymap.lua
@@ -4,19 +4,20 @@
 -- C - <Ctrl>
 -- Space - <Space>
 
-lvim.keys.normal_mode["<C-M-v>"] = "<Cmd>lua require('jdtls').extract_variable()<CR>"
-lvim.keys.normal_mode["<C-M-c>"] = "<Cmd>lua require('jdtls').extract_constant()<CR>"
-lvim.keys.normal_mode["<C-M-o>"] = "<Cmd>lua require('jdtls').organize_imports()<CR>"
-lvim.keys.normal_mode["<C-M-m>"] = "<Cmd>lua require('jdtls').extract_method()<CR>"
-lvim.keys.normal_mode["<F5>"] = "<Cmd>terminal mvn eclipse:eclipse<CR>"
-lvim.keys.normal_mode["<F7>"] = "<Cmd>lua require('dapui').toggle({reset = true})<CR>"
-lvim.keys.normal_mode["<F8>"] = "<Cmd>lua require('dap').step_over()<CR>"
-lvim.keys.normal_mode["<F9>"] = "<Cmd>lua require('dap').continue()<CR>"
-lvim.keys.normal_mode["<F10>"] = "<Cmd>lua require('dap').disconnect()<CR>"
+-- LSP, DAP and Java mappings
+vim.keymap.set("n", "<C-M-v>", "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = "Extract variable Java" })
+vim.keymap.set("n", "<C-M-c>", "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = "Extract constant Java" })
+vim.keymap.set("n", "<C-M-o>", "<Cmd>lua require('jdtls').organize_imports()<CR>", { desc = "Organize imports Java" })
+vim.keymap.set("n", "<C-M-m>", "<Cmd>lua require('jdtls').extract_method()<CR>", { desc = "Extract method Java" })
+vim.keymap.set("n", "<F5>", "<Cmd>terminal mvn clean dependency:resolve<CR>", { desc = "Refresh maven dependencies" })
+vim.keymap.set("n", "<F7>", "<Cmd>lua require('dapui').toggle({reset = true})<CR>", { desc = "Toggle DAP UI" })
+vim.keymap.set("n", "<F8>", "<Cmd>lua require('dap').step_over()<CR>", { desc = "Debug step over" })
+vim.keymap.set("n", "<F9>", "<Cmd>lua require('dap').continue()<CR>", { desc = "Debug continue" })
+vim.keymap.set("n", "<F10>", "<Cmd>lua require('dap').disconnect()<CR>", { desc = "Debug stop" })
 
 -- Bufferline mappings
-lvim.keys.normal_mode["<M-l>"] = "<Cmd>BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<M-h>"] = "<Cmd>BufferLineCyclePrev<CR>"
+vim.keymap.set("n", "<M-l>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<M-h>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
 
 -- ChatGPT mappings
 lvim.builtin.which_key.mappings["a"] = {
