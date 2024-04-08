ec391f9b792da1fb609da94d9985c63d9dc5220f
Israiloff <stardustindark@gmail.com>
2024-04-08 14:32:42 +0500

    added: markdown preview feature;

---
+++ lua/local/markdown-preview.lua
@@ -0,0 +1,5 @@
+vim.g.mkdp_open_to_the_world = 1
+vim.g.mkdp_open_ip = '127.0.0.1'
+vim.g.mkdp_port = 33235
+vim.g.mkdp_browser = 'none'
+vim.g.mkdp_echo_preview_url = 1
--- lua/local/plugins.lua
+++ lua/local/plugins.lua
@@ -38,5 +38,13 @@ lvim.plugins = {
     },
     {
         "archibate/lualine-time"
-    }
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
